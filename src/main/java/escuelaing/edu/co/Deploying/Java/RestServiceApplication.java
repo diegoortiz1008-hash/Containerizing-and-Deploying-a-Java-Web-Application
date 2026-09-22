5829216257ba275ab2c336744895184a229418fd
unknown <diego.ortiz-s@labinfo.is.escuelaing.edu.co>
2026-09-22 10:21:06 -0500

    Docker config

---
+++ src/main/java/escuelaing/edu/co/Deploying/Java/RestServiceApplication.java
@@ -0,0 +1,21 @@
+package escuelaing.edu.co.Deploying.Java;
+
+import java.util.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication application =
+                new SpringApplication(RestServiceApplication.class);
+
+        application.setDefaultProperties(
+                Map.of("server.port",
+                        System.getenv().getOrDefault("PORT", "9000")));
+
+        application.run(args);
+    }
+}
