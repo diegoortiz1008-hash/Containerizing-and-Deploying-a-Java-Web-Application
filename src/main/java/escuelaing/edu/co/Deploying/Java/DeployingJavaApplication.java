71ff1eafd0f3d181c4d685edf4648744eae70270
unknown <diego.ortiz-s@labinfo.is.escuelaing.edu.co>
2026-09-22 09:18:20 -0500

    initial sprint

---
+++ src/main/java/escuelaing/edu/co/Deploying/Java/DeployingJavaApplication.java
@@ -0,0 +1,13 @@
+package escuelaing.edu.co.Deploying.Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployingJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployingJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/escuelaing/edu/co/Deploying/Java/DeployingJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package escuelaing.edu.co.Deploying.Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployingJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
